1358fc4389489b43d5f4ab9e9a6bb1a8fe3cd3d3
Felipe Costa <fcostax02@gmail.com>
2016-04-15 20:03:01 -0300

    Modelo Produto e Tela de inserir Produto (incompleta).

---
+++ app/src/main/java/br/com/interaje/interaje01/InsertProductActivity.java
@@ -0,0 +1,13 @@
+package br.com.interaje.interaje01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_product);
+    }
+}
---
+++ app/src/main/java/br/com/interaje/interaje01/model/Category.java
@@ -0,0 +1,7 @@
+package br.com.interaje.interaje01.model;
+
+/**
+ * Created by rayquaza on 15/04/16.
+ */
+public class Category {
+}
